51cb92fab691cff982e80c9e0004d76cfa43fcdc
priyalead <90104010+priyalead@users.noreply.github.com>
2021-09-04 22:14:42 +0530

    positive or negative

---
+++ Assignment2.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		int num = 0;
+
+//		num = Math.sign(num);
+
+		if (num > 0) {
+			System.out.println("Positive");
+		} else if (num < 0) {
+			System.out.println("Negative");
+		} else {
+			System.out.println("Nethier Positive nor negative");
+		}
+
+	}
+}
