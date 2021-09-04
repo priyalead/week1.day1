132655690ef185daaeb0569a048dee9e9c3ea630
priyalead <90104010+priyalead@users.noreply.github.com>
2021-09-04 21:44:11 +0530

    first assigment

---
+++ Assigmnt1.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Assigmnt1 {
+
+	public static void main(String[] args) {
+		int num = -20, result;
+
+		if (num < 0) {
+			result = num * -1;
+					
+			System.out.println("The number "+ num + " is converted to " + result);
+		} 
+
+	}
+
+}
